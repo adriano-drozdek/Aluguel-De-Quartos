512f6c0455a56c63722b5a243fc623a648e02a34
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-09 15:46:04 -0300

    Adicionando constructor using fields.

--- src/entities/ImpleAluguel.java
+++ src/entities/ImpleAluguel.java
@@ -2,4 +2,20 @@ package entities;
 
 public class ImpleAluguel {
 
+	private int aluguel;
+	private String nome;
+	private String email;
+	private int quarto;
+
+	public ImpleAluguel(int aluguel, String nome, String email, int quarto) {
+		this.aluguel = aluguel;
+		this.nome = nome;
+		this.email = email;
+		this.quarto = quarto;
+	}
+
+	public ImpleAluguel() {
+
+	}
+
 }
