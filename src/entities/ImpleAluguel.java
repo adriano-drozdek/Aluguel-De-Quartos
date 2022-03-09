22e8f4bd64bbb34860597ae076e9da37caaa45ba
Adriano Drozdek <adrianodrozdek2791@gmail.com>
2022-03-09 15:47:58 -0300

    Adicionando getters/setters.

--- src/entities/ImpleAluguel.java
+++ src/entities/ImpleAluguel.java
@@ -18,4 +18,36 @@ public class ImpleAluguel {
 
 	}
 
+	public int getAluguel() {
+		return aluguel;
+	}
+
+	public void setAluguel(int aluguel) {
+		this.aluguel = aluguel;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getQuarto() {
+		return quarto;
+	}
+
+	public void setQuarto(int quarto) {
+		this.quarto = quarto;
+	}
+
 }
